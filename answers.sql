08d0d2be47b225c0fadc881c385e30a70b53744d
Vuthlari0116 <kvmabaso2001@gmail.com>
2025-05-04 20:41:07 +0200

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -15,14 +15,6 @@ FROM
     ProductDetail,
     STRING_SPLIT(Products, ',') AS products;
 
--- Alternatively, if your database management system doesn't support STRING_SPLIT,
--- you might need to use a different approach, such as a recursive query or a custom function.
-
--- Drop the original table (optional)
--- DROP TABLE ProductDetail;
-
--- Rename the new table to the original table name (optional)
-
 
 
 - Create a new table to store order information
